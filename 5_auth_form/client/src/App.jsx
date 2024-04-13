198fd263df49769badacf66a455eceb203299f08
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-04-13 18:18:51 +0000

    New folder added

---
+++ 5_auth_form/client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from './pages/Signup'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
+
+function App() {
+  
+
+  return (
+    
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
